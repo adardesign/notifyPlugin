47d33738bce3e52b2012bda21f5b6c7c5933cd2a
Eliazer <adardesign@gmail.com>
2012-12-09 12:11:55 -0500

    started new branch

--- JS/notify.js
+++ JS/notify.js
@@ -1,49 +1,56 @@
- var notify = {
-     defaults: {
-         location: {
-             top: 0,
-             left: 0
-         },
-         autoFadeOut: true,
-         fadesAfter: 5000,
-         fadeInSpeed: 300,
-         fadeOutSpeed:300,
-         className: "note",
-         relatedEvent: {}
+ var Notify = function() {};
+ Notify.prototype.defaults = {
+     location: {
+         top: 0,
+         left: 0
      },
-     add: function(content, config){
-         //single var pattern;
-         var self = this,
-             options = $.extend({}, this.defaults, config),
-             notification, timeoutId, sendTofadeOut, cancelFadeOut, relatedEvent, jRelatedEvent, relatedWidth, relatedHeight, thisHeight, thisWidth, viewPort = {
-                 x: $(window).width(),
-                 y: $(window).height()
-             };
-         notification = $("<div>", {
-             "class": "notification " + options.className,
-             "html": content
-         }).appendTo("body").fadeTo(options.fadeInSpeed, 1).hover(function() {
-             cancelFadeOut();
-         }, function() {
-             sendTofadeOut(100);
-         });
-         // set position
-         notification.css(options.location);
-         sendTofadeOut = function(speed) {
-             timeoutId = setTimeout(function() {
-                 self.fadeRemove(notification, options.fadeOutSpeed);
-             }, (speed ? speed : options.fadesAfter));
+     autoFadeOut: true,
+     fadesAfter: 5000,
+     fadeInSpeed: 300,
+     fadeOutSpeed: 300,
+     className: "note",
+     relatedEvent: {}
+ };
+
+Notify.prototype.fadeRemove = function (id, speed) {
+     id.fadeOut(speed, function() {
+         id.remove();
+     });
+ };
+
+ Notify.prototype.add = function(content, config) {
+     //single var pattern;
+     var self = this,
+         options = $.extend({}, this.defaults, config),
+         notification, timeoutId, sendTofadeOut, cancelFadeOut, relatedEvent, jRelatedEvent, relatedWidth, relatedHeight, thisHeight, thisWidth, viewPort = {
+             x: $(window).width(),
+             y: $(window).height()
          };
-         cancelFadeOut = function() {
-             clearTimeout(timeoutId);
-         };
-         if(options.autoFadeOut) {
-             sendTofadeOut();
-         }
-     },
-     fadeRemove: function(id, speed) {
-         id.fadeOut(speed, function() {
-             id.remove();
-         });
+     notification = $("<div>", {
+         "class": "notification " + options.className,
+         "html": content
+     }).appendTo("body").fadeTo(options.fadeInSpeed, 1).hover(function() {
+         cancelFadeOut();
+     }, function() {
+         sendTofadeOut(100);
+     });
+     // set position
+     notification.css(options.location);
+     sendTofadeOut = function(speed) {
+         timeoutId = setTimeout(function() {
+             self.fadeRemove(notification, options.fadeOutSpeed);
+         }, (speed ? speed : options.fadesAfter));
+     };
+     cancelFadeOut = function() {
+         clearTimeout(timeoutId);
+     };
+     if(options.autoFadeOut) {
+         sendTofadeOut();
      }
  };
+
+
+
+var note = new Notify({  // new instance
+
+});
